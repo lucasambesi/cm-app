05441d7f226224a7eb0fa8c28e626b805e6d13ff
Lucas Ambesi <ambesilucas@gmail.com>
2022-05-15 20:11:22 -0300

    add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+const MODE = {
+	OFF: 'off',
+	WARN: 'warn',
+	ERROR: 'error'
+}
+
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "google"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
+        'react/react-in-jsx-scope': MODE.OFF,
+		'require-jsdoc': MODE.OFF,
+		'no-unused-vars': MODE.OFF,
+        "semi": [2, "never"]
+    }
+}
