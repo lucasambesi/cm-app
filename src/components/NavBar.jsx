a6c635a8b5bf2b2d95c39c8eb63d18aaf13064e1
Lucas Ambesi <ambesilucas@gmail.com>
2022-05-17 02:43:48 -0300

    migration NavBar mui

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,39 +1,187 @@
 import React from 'react'
-import {Container, Nav, Navbar, NavDropdown} from 'react-bootstrap'
 
-export const NavBar = () => {
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import Menu from '@mui/material/Menu'
+import Container from '@mui/material/Container'
+import Avatar from '@mui/material/Avatar'
+import Button from '@mui/material/Button'
+import Tooltip from '@mui/material/Tooltip'
+import MenuItem from '@mui/material/MenuItem'
+import MenuIcon from '@mui/icons-material/Menu'
+import AdbIcon from '@mui/icons-material/Adb'
+
+export const NavBar = ({lang = 'es'}) => {
+    const [anchorElNav, setAnchorElNav] = React.useState(null)
+    const [anchorElUser, setAnchorElUser] = React.useState(null)
+
+    const translate = {
+        es: {
+            appName: 'Crypto Manager',
+            pages: ['Mercados', 'Portafolio'],
+            settingsTab: 'Abrir ajustes',
+            settings: ['Perfil', 'Cuenta', 'Salir'],
+        },
+        en: {
+            appName: 'Crypto Manager',
+            pages: ['Markets', 'Portfolio'],
+            settingsTab: 'Open settings',
+            settings: ['Profile', 'Account', 'Logout'],
+        },
+    }
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget)
+    }
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget)
+    }
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null)
+    }
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null)
+    }
+
     return (
-        <Navbar bg="light" expand="lg">
-            <Container>
-                <Navbar.Brand href="#home">Portfolio Manager</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#markets">Markets cryptos</Nav.Link>
-                        <NavDropdown title="Portfolio" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#portfolio/3.1">
-                                Binance
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#portfolio/3.2">
-                                OKEx
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#portfolio/3.3">
-                                Metamask
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#portfolio/3.4">
-                                Defi
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-                <Navbar.Collapse className="justify-content-end">
-                    <Navbar.Text>
-                        Signed in as: <a href="#login">User</a>
-                    </Navbar.Text>
-                </Navbar.Collapse>
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <AdbIcon sx={{display: {xs: 'none', md: 'flex'}, mr: 1}} />
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: {xs: 'none', md: 'flex'},
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        {translate[lang].appName}
+                    </Typography>
+
+                    <Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: {xs: 'block', md: 'none'},
+                            }}
+                        >
+                            {translate[lang].pages.map((page) => (
+                                <MenuItem
+                                    key={page}
+                                    onClick={handleCloseNavMenu}>
+                                    <Typography
+                                        textAlign="center">{page}
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <AdbIcon sx={{display: {xs: 'flex', md: 'none'}, mr: 1}} />
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href=""
+                        sx={{
+                            mr: 2,
+                            display: {xs: 'flex', md: 'none'},
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        {translate[lang].appName}
+                    </Typography>
+                    <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
+                        {translate[lang].pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                sx={{my: 2, color: 'white', display: 'block'}}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
+
+                    <Box sx={{flexGrow: 0}}>
+                        <Tooltip title={translate[lang].settingsTab}>
+                            <IconButton
+                                onClick={handleOpenUserMenu}
+                                sx={{p: 0}}>
+                                <Avatar
+                                    alt=""
+                                    src="/assets/profile.png"
+                                />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{mt: '45px'}}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {translate[lang].settings.map((setting) => (
+                                <MenuItem
+                                    key={setting}
+                                    onClick={handleCloseUserMenu}>
+                                    <Typography
+                                        textAlign="center">{setting}
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
             </Container>
-        </Navbar>
+        </AppBar>
     )
 }
