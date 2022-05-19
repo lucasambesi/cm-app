9cd3d73d4685135454b02b3353a232742365ce35
Lucas Ambesi <ambesilucas@gmail.com>
2022-05-19 20:15:29 -0300

    add LangContext Provider and lang router

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,44 @@
+import React, {useState} from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
 import {Markets} from './components/Markets'
 import {NavBar} from './components/NavBar'
+import {LangContext} from './hooks/langContext'
 
 export const App = () => {
+    const [lang, setLang] = useState('en')
+
+    const translate = {
+        es: {
+            markets: 'Mercados',
+            portfolio: 'Portafolio',
+        },
+        en: {
+            markets: 'Markets',
+            portfolio: 'Portfolio',
+        },
+    }
+
     return (
         <>
-            <NavBar/>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Markets/>}/>
-                </Routes>
-            </BrowserRouter>
+            <LangContext.Provider value={{
+                lang,
+            }}>
+                <NavBar setLang={setLang}/>
+                <BrowserRouter>
+                    <Routes>
+                        <Route
+                            path={translate[lang].markets}
+                            element={<Markets/>}
+                        />
+                        <Route
+                            path={translate[lang].portfolio}
+                            element={<h1>Portfolio</h1>}
+                        />
+                        <Route path="/" element={<Markets/>}/>
+                    </Routes>
+                </BrowserRouter>
+            </LangContext.Provider>
         </>
     )
 }
