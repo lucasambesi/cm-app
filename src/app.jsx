6046ead395f4dca9e85b4a51dbbde53870cfa7be
Lucas Ambesi <ambesilucas@gmail.com>
2022-05-22 12:38:38 -0300

    update Router

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,22 @@
 import React, {useState} from 'react'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 import {Markets} from './components/Markets'
 import {NavBar} from './components/NavBar/NavBar'
+import {Overview} from './components/Portfolio/Crypto/Overview/Overview'
 import {LangContext} from './hooks/langContext'
 
 export const App = () => {
     const [lang, setLang] = useState('en')
 
     const paths = {
+        default: '/',
         markets: 'markets',
-        portfolio: 'portfolio',
+        portfolio: {
+            crypto: 'crypto',
+            actions: 'actions',
+        },
+
     }
 
     return (
@@ -18,20 +24,23 @@ export const App = () => {
             <LangContext.Provider value={{
                 lang,
                 setLang}}>
-                <NavBar/>
-                <BrowserRouter>
+                <Router>
+                    <NavBar/>
                     <Routes>
                         <Route
                             path={paths.markets}
                             element={<Markets/>}/>
                         <Route
-                            path={paths.portfolio}
-                            element={<h1>Portfolio</h1>}/>
+                            path={paths.portfolio.crypto}
+                            element={<Overview/>}/>
                         <Route
-                            path="/"
+                            path={paths.portfolio.actions}
+                            element={<h1>Actions</h1>}/>
+                        <Route
+                            path={paths.default}
                             element={<Markets/>}/>
                     </Routes>
-                </BrowserRouter>
+                </Router>
             </LangContext.Provider>
         </>
     )
