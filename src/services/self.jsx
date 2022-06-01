17a77f09690b507a55f3e828051fd08ce68b9ee3
Lucas Ambesi <ambesilucas@gmail.com>
2022-06-01 00:43:45 -0300

    dispatch logout

--- src/services/self.jsx
+++ src/services/self.jsx
@@ -1,4 +1,19 @@
+import {useContext} from 'react'
+import {useNavigate} from 'react-router-dom'
+import {AuthContext} from '../auth/authContext'
+import {types} from '../types/types'
+
 export const logout = () => {
-    localStorage.clear()
-    if ( window.location.pathname !== '/login' ) window.location = '/login'
+    const navigate = useNavigate()
+    const {dispatch} = useContext(AuthContext)
+
+    dispatch({
+        type: types.logout,
+    })
+
+    navigate('login', {
+        replace: true,
+    })
+    // localStorage.clear()
+    // if ( window.location.pathname !== '/login' ) window.location = '/login'
 }
